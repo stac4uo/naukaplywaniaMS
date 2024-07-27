35c522b047d46a68767f62eb25020008042eff40
stac4uo <86820086+stac4uo@users.noreply.github.com>
2024-07-27 19:42:14 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 let burgerIcon = document.getElementById('burgerIcon')
 let mobilMenu = document.getElementById('mobileMenu')
 let closeIcon = document.getElementById('closeIcon')
@@ -30,3 +31,37 @@ closeIcon.addEventListener('click',()=>{
         }
     }
 
+=======
+let burgerIcon = document.getElementById('burgerIcon')
+let mobilMenu = document.getElementById('mobileMenu')
+let closeIcon = document.getElementById('closeIcon')
+let main = document.querySelector('main')
+
+
+
+
+burgerIcon.addEventListener('click',()=>{
+mobilMenu.classList.toggle('hidden')
+main.classList.toggle('hidden')
+
+})
+
+closeIcon.addEventListener('click',()=>{
+    mobilMenu.classList.toggle('hidden')
+    main.classList.toggle('hidden')
+    })
+    
+
+    function toggleAnswer(element) {
+        const answer = element.nextElementSibling;
+        const icon = element.querySelector('span:last-child');
+        if (answer.classList.contains('hidden')) {
+            answer.classList.remove('hidden');
+            icon.textContent = '-';
+        } else {
+            answer.classList.add('hidden');
+            icon.textContent = '+';
+        }
+    }
+
+>>>>>>> d8c76a6ed9c1b419a886395f97bebdd4f7475ce8
